3ec98a6e150f7e412eacfb55b8494b1a56368603
Ataxia123 <vp.ca94@gmail.com>
2023-07-06 22:04:31 -0500

    updated todo

--- packages/nextjs/components/NerdTodo.tsx
+++ packages/nextjs/components/NerdTodo.tsx
@@ -2,7 +2,7 @@ import { Card } from "@mui/material";
 
 export default function ToDo() {
   return (
-    <Card className="flex flex-col justify-center items-center p-5 m-5 w-96 h-96 rounded-xl shadow-xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-black font-bold py-2 px-6 rounded">
+    <Card className="flex flex-col text-left p-5 m-5 w-196 h-96 rounded-xl shadow-xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-black font-bold py-2 px-6 rounded overflow-X-scroll">
       ToDo List
       <ul className="list-disc list-inside">
         <li>
@@ -11,15 +11,24 @@ export default function ToDo() {
         </li>
 
         <li>
-          {" "}
-          Graph Query: Redeploy graph and fetch active positions
-          <ul className="">
-            <li>
-              <span style={{ fontWeight: "bold" }}>**NEW***</span> useScaffold contract listener instead
-            </li>
+          useScaffold contract listener instead<span style={{ color: "red" }}>DONE</span>
+        </li>
+        <li>
+          Setup Card component[at0x]
+          <ul>
+            <li>Import Setup Card component</li>
+            <li>Fix: Calculate liquidity ammounts</li>
+            <li>Import Approval flow</li>
           </ul>
         </li>
-        <li> Add Liquidity function []</li>
+        <li>
+          {" "}
+          Add Liquidity function []
+          <ul className="">
+            <li>define function array</li>
+            <li>manage users with and without existing positions</li>
+          </ul>
+        </li>
         <li> Remove Liquidity function []</li>
         <li> Claim FUnction []</li>
       </ul>
